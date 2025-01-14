4f577239bfd5222464be8eadd5dac02fb5de5ae2
celj <kaelyptic@outlook.com>
2025-01-14 16:01:00 +0800

    update config and package files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,css}", // This ensures all your JS and JSX files are scanned
+    "./src/**/*.{html,js,jsx,ts,tsx,css}", // Includes CSS files in the src directory
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
